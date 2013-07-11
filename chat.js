e4e965d2801dc4d7f3fae8f7e5d93cd86c938d7c
Geoff Petrie <g.petrie@gmail.com>
2013-07-10 19:50:25 -0600

    Add socket checks
    We're checking for socket closure now. Also adding a timestamp to the
    conversation.

--- chat.js
+++ chat.js
@@ -7,21 +7,41 @@ var chatServer = net.createServer(),
                   clientList = []
 
 chatServer.on('connection', function(client) {
+
   client.name = client.remoteAddress + ':' + client.remotePort
-  client.write('Hi! ' + client.name + '!\n');
+  client.write('Hey there ' + client.name + '!\n');
 
   clientList.push(client)
 
   client.on('data', function(data) {
     broadcast(data, client)
   })
+
+  client.on('end', function() {
+    console.log(client.name + ' quit')
+    clientList.splice(clientList.indexOf(client), 1)
+  })
+
+  client.on('error', function(e) {
+    console.log(e)
+  })
 })
 
 function broadcast(message, client) {
+  var cleanup = []
+  var timeStuff = new Date()
   for (var i = 0; i < clientList.length; i += 1) {
     if (client !== clientList[i]) {
-      clientList[i].write(client.name + " says " + message)
+      if (clientList[i].writable) {
+        clientList[i].write(timeStuff.toTimeString() + ' ' + client.name + ' says: ' + message)
+      } else {
+        cleanup.push(clientList[i])
+        clientList[i].destroy()
+      }
     }
+  }
+  for (i = 0; i < cleanup.length; i += 1) {
+    clientList.splice(clientList.indexOf(cleanup[i]), 1)
   }
 }
 
